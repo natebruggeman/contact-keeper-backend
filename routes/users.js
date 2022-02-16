e3957f1b66e8121d21f5bbb6364983d1aed4dca3
Nate Bruggeman <njbruggeman@gmail.com>
2022-02-16 16:37:24 -0600

    responding to user post request with JWT

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { reset } = require("nodemon");
 
 const User = require("../models/User");
@@ -28,12 +30,12 @@ router.post(
     const { name, email, password } = req.body;
 
     try {
-      let user = await User.findOne({ email });
+      let user = await User.findOne({ email }); //finding if email exists in the db
 
       if (user) {
         return res.status(400).json({ msg: "This user already exists" });
       }
-      //creating a new instance of a user,
+      //creating a new instance of a user
       user = new User({
         name,
         email,
@@ -49,7 +51,25 @@ router.post(
       //saving the name, email, and hashed password to the db
       await user.save();
 
-      res.send("user saved");
+      // creating payload, all we need is id because we can get the rest of info with just id
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      //
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
