2ac0e5552e424564cd5fcf3f71e8a09c41aee850
Nate Bruggeman <njbruggeman@gmail.com>
2022-02-16 11:05:37 -0600

    added dummie routes, working on postman

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express"); //can't use import without babel or typescript
+
+const app = express();
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the Contact Keeper API..." })
+);
+
+//define routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+const PORT = process.env.PORT || 5000; //using 5000 in development, process.env is for production
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
